1c4bb21cb13d2a01e8494e0cc3e7e27f8fe33391
Alex Calaca <alexcalaca@gmail.com>
2018-06-09 04:29:55 +0000

    Change room routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   devise_for :users
+  
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
