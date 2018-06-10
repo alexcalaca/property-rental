20b85d918c6b534e7c5e2acc40f91b0bab4f1f27
Alex Calaca <alexcalaca@gmail.com>
2018-06-10 03:27:32 +0000

    Update room validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,10 +4,8 @@ class Room < ApplicationRecord
   validates :home_type, presence: true
   validates :room_type, presence: true
   validates :accommodate, presence: true
-  validates :bed_room, presence: true
-  validates :bath_room, presence: true
-  validates :listing_name, presence: true, length: {maximum: 50}
-  validates :summary, presence: true, length: {maximum: 500}
-  validates :address, presence: true
+  validates :bedroom, presence: true
+  validates :bathroom, presence: true
+  
 end
 
