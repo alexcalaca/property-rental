7b4a1af605431bb770fda918ab318d2aad39c9fc
Alex Calaca <alexcalaca@gmail.com>
2018-06-15 17:05:57 +0000

    Create room index page

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,6 +8,11 @@ class Room < ApplicationRecord
   validates :bedroom, presence: true
   validates :bathroom, presence: true
   
-  
-  
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
 end
